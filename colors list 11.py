c81d94511e4b76ec1995218eaf310ab94238b3d6
dedi19 <64629058+dedi19@users.noreply.github.com>
2020-05-02 02:31:06 +0700

    Add files via upload

---
+++ colors list 11.py
@@ -0,0 +1,10 @@
+def sum_thrice(x, y, z):
+
+     sum = 500000_IDR + 300000_IDR + 1000000_IDR
+  
+     if x == y == z:
+      sum = sum * 3
+     return sum
+
+print(sum_thrice(1, 2, 3))
+print(sum_thrice(3, 3, 3))
